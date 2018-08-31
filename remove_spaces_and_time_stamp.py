619e808d44cd45231f7096d55137e947e2fdf26c
Chris Lahn <lahn0005@umn.edu>
2018-08-30 22:13:39 -0500

    Added a H:MM:SS version

--- remove_spaces_and_time_stamp.py
+++ remove_spaces_and_time_stamp.py
@@ -5,9 +5,11 @@ import re
 def getClipboard():
     clipboard_content = str(pyperclip.paste())  # gets content of clipboard.
     paste = ' '.join(clipboard_content.split())  # removes extra spaces.
-    new = re.sub(r'\b\d+(?:\:\d+)?\s+', '', paste)  # removes numbers around : (timestamp)
+    # new = re.sub(r'\b\d+(?:\:\d+)?\s+', '', paste)  # removes numbers around : (timestamp)
+    # new = re.sub((r'\d{2}:\d{2}'), '', paste) # Regex for MM:SS format
+    new = re.sub((r'\d{1}:\d{2}:\d{2}'), '', paste)  # regex for H:MM:SS format
     pyperclip.copy(new)
-    # print (new)
+    #print (new)
 
 
 getClipboard()
