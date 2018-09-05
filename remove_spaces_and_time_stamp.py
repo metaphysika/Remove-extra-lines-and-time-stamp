2428f48d4cdfb6960fbe7986a1d57628d387f43c
Chris Lahn <lahn0005@umn.edu>
2018-09-04 21:50:32 -0500

    Added condition for 1 or 2 digits in hour spot
    {1,2}

--- remove_spaces_and_time_stamp.py
+++ remove_spaces_and_time_stamp.py
@@ -7,7 +7,7 @@ def getClipboard():
     paste = ' '.join(clipboard_content.split())  # removes extra spaces.
     # new = re.sub(r'\b\d+(?:\:\d+)?\s+', '', paste)  # removes numbers around : (timestamp)
     # new = re.sub((r'\d{2}:\d{2}'), '', paste) # Regex for MM:SS format
-    new = re.sub((r'\d{1}:\d{2}:\d{2}'), '', paste)  # regex for H:MM:SS format
+    new = re.sub((r'\d{1,2}:\d{2}:\d{2}'), '', paste)  # regex for H:MM:SS format
     pyperclip.copy(new)
     #print (new)
 
